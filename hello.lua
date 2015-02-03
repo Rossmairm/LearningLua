e8d91e03754d00544bb80bf2273a5d264133601e
U-Mark-PC\Mark <Mark@Mark-PC.(none)>
2015-02-03 01:54:28 -0500

    basic bitch

--- hello.lua
+++ hello.lua
@@ -1,10 +1,11 @@
-function helloworld(text)
-	if text == null then
-        	print ("enter a number")
-                input = io.read("*string")
+--function helloworld(text) 
+	if input == null then
+        	print ("enter a numiber")
+	                input = io.read()
 		print (input)
+
 	else
 		print(text)
 	end
-end
+--end
 
